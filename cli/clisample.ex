2c4b95481bf70650a90fc3ae2383dec2d3f79382
tbpgr <tbpgr@tbpgr.jp>
2014-11-26 02:59:14 +0900

    CLI sample

---
+++ cli/clisample.ex
@@ -0,0 +1,25 @@
+defmodule Clisample do
+  def main(args) do
+    IO.inspect(fizzbuzz(String.to_integer(List.first(args)), String.to_integer(List.last(args))))
+  end
+
+  @doc ~S"""
+  FizzBuzz.
+
+  ## Examples
+
+      iex> Clisample.fizzbuzz(1, 15)
+      [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14
+, "FizzBuzz"]
+  """
+  def fizzbuzz(from, to) when is_integer(from) and is_integer(to) do
+    from..to |> Enum.map(&(fizzbuzz/1))
+  end
+  def fizzbuzz(_from, _to) do
+    raise "invalid argument"
+  end
+  defp fizzbuzz(n) when rem(n, 15) == 0 , do: "FizzBuzz"
+  defp fizzbuzz(n) when rem(n, 3) == 0, do: "Fizz"
+  defp fizzbuzz(n) when rem(n, 5) == 0, do: "Buzz"
+  defp fizzbuzz(n), do: n
+end
